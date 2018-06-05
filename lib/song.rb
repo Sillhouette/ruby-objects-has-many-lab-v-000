70876cf2f16ceb618a686647735f4ca1a903f22e
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 23:04:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,18 @@ class Song
 
   attr_accessor :title, :artist
 
+  @@all = [] 
+
   def initialize(title)
     @title = title;
   end
 
   def artist_name
     self.artist.name;
+  end
+
+  def self.song_count
+    @@all;
   end
 
 end
