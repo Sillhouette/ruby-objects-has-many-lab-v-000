efb0dfaf4e58b82bfc62ea892453c3af8ab3c495
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 23:16:51 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,27 @@
-class Author
+class Artist
 
-  attr_accessor :name, :post
+  attr_accessor :name, :posts
 
-  def initialize
+  @@all = 0;
 
+  def initialize(name)
+    @name = name;
+    @posts = [];
+  end
+
+  def add_post(post)
+    @posts << post;
+    post.author = self;
+  end
+
+  def add_post_by_name(name)
+    post = Post.new(name);
+    @posts << post;
+    post.author = self;
+  end
+
+  def self.song_count
+    @posts.length
   end
 
 end
